cd8892d4799900a16d0adcb7697f238185e3a9f9
Carlo <cruz.carlo.diaz@gmail.com>
2024-05-31 23:09:58 +0800

    FCC Request Header Parser Microservice solution

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+var fetch = require('node-fetch');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -22,6 +23,15 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', async function (req, res) {
+  const responseIp = await fetch('https://api.ipify.org?format=json');
+  const ipObj = await responseIp.json();
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({ ipaddress: ipObj.ip, language, software });
 });
 
 // listen for requests :)
